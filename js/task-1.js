03b9b10856faa7f1777ea4aa4d768bee19a72e93
Oleksandr-Kalinin <work.kalinin.o.v@gmail.com>
2024-04-03 00:23:15 +0300

    modify

--- js/task-1.js
+++ js/task-1.js
@@ -1,7 +1,5 @@
 'use strict';
 
-// homework js 2
-
 function makeTransaction(quantity, pricePerDroid, customerCredits) {
     let totalPriceOrder = quantity * pricePerDroid;
     if (totalPriceOrder > customerCredits) {
@@ -17,15 +15,4 @@ console.log(makeTransaction(10, 5000, 8000)); // "Insufficient funds!"
 console.log(makeTransaction(8, 2000, 10000)); // "Insufficient funds!"
 console.log(makeTransaction(10, 500, 5000)); // "You ordered 10 droids worth 5000 credits!"
 
-/*
-// homework js 1
-
-function makeTransaction(quantity, pricePerDroid) {
-    return `You ordered ${quantity} droids worth ${quantity * pricePerDroid} credits!`;
-}
-
-console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
-console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
-console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
-*/
 
--- js/task-2.js
+++ js/task-2.js
@@ -1,7 +1,5 @@
 'use strict';
 
-// homework js 2
-
 function formatMessage(message, maxLength) {
     if (message.length <= maxLength) {
         return message;
@@ -16,15 +14,3 @@ console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum
 console.log(formatMessage("Vestibulum facilisis purus nec", 30)); // "Vestibulum facilisis purus nec"
 console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); // "Nunc sed turpis..."
 console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); // "Nunc sed turpis a felis in nunc fringilla"
-
-/*
-// homework js 1
-
-function getShippingMessage(country, price, deliveryFee) {
-    return `Shipping to ${country} will cost ${price + deliveryFee} credits`;
-}
-
-console.log(getShippingMessage("Australia", 120, 50)); // "Shipping to Australia will cost 170 credits"
-console.log(getShippingMessage("Germany", 80, 20)); // "Shipping to Germany will cost 100 credits"
-console.log(getShippingMessage("Sweden", 100, 20)); // "Shipping to Sweden will cost 120 credits"
-*/
